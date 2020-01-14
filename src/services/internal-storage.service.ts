13b3bcf0be0d2ff2af07b25db2edcacb98ca526e
Martin <mendozanmartin@gmail.com>
2020-01-14 15:02:30 -0500

    bug fixes

--- src/services/internal-storage.service.ts
+++ src/services/internal-storage.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Couchbase } from 'nativescript-couchbase-plugin';
+const database = new Couchbase('my-database');
 
 @Injectable({
   providedIn: 'root'
@@ -6,5 +8,21 @@ import { Injectable } from '@angular/core';
 export class InternalStorageService {
 
   constructor() { }
+
+  createDocument(name: string) {
+    return database.createDocument(name)
+  }
+
+  deleteDocument(name: string) {
+    return database.deleteDocument(name)
+  }
+
+  getDocument(name: string) {
+    return database.getDocument(name)
+  }
+
+
+
+
    
 }
