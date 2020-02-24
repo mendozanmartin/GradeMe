8fff825c8c88b5492ea746d09df2cc644e8b1009
Martin <mendozanmartin@gmail.com>
2020-02-24 17:40:34 -0500

    get courses using couchbase

--- src/services/auth-guard.service.ts
+++ src/services/auth-guard.service.ts
@@ -1,27 +1,20 @@
-import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
-import * as firebase from "nativescript-plugin-firebase";
-import { PersistentSettings } from './persistent-settings.service';
-import { RouterExtensions } from 'nativescript-angular/router';
+import { Injectable } from "@angular/core";
+import { CanActivate } from "@angular/router";
+import { PersistentSettings } from "./persistent-settings.service";
+import { RouterExtensions } from "nativescript-angular/router";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: "root"
 })
 export class AuthGuardService implements CanActivate {
+    constructor(private router: RouterExtensions) {}
 
-  constructor(private router: RouterExtensions) { }
-
-  canActivate() {
-    if (PersistentSettings.isLoggedIn()) {
-      return true
-    } else {
-      this.router.navigate(["/login"])
-      return false
+    canActivate() {
+        if (PersistentSettings.isLoggedIn()) {
+            return true;
+        } else {
+            this.router.navigate(["/login"]);
+            return false;
+        }
     }
-  }
-  
-  createAccount() {
-  
-  }
-  
 }
--- src/services/firestore.service.ts
+++ src/services/firestore.service.ts
@@ -1,24 +1,38 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import * as firebase from "nativescript-plugin-firebase";
-import { User } from '~/models/user.model';
-
+import { User } from "~/models/user.model";
+import { InternalStorageService } from "./internal-storage.service";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: "root"
 })
 export class FirestoreService {
-  private usersCollection: firebase.firestore.CollectionReference;
+    private usersCollection: firebase.firestore.CollectionReference;
+    private coursesCollection: firebase.firestore.CollectionReference;
 
-  constructor() { 
-    this.usersCollection = firebase.firestore.collection("Users");
-  }
+    constructor(private storage: InternalStorageService) {
+        this.usersCollection = firebase.firestore.collection("Users");
+        this.coursesCollection = firebase.firestore.collection("Courses");
+    }
 
-  updateUser(user: User) {
-    this.usersCollection.doc(user.uid).set({
-      firstName: user.firstName,
-      lastName: user.lastName,
-      email: user.email,
-      uid: user.uid
-    })
-  }
+    updateUser(user: User) {
+        this.usersCollection.doc(user.uid).set({
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            uid: user.uid
+        });
+    }
+
+    updateStorageCourses(uid: String) {
+        const query = this.coursesCollection.where("uid", "==", uid);
+        query
+            .get()
+            .then(querySnapshot => {
+                querySnapshot.forEach(doc => {
+                    this.storage.syncCourses(doc.data());
+                });
+            })
+            .catch(console.error);
+    }
 }
