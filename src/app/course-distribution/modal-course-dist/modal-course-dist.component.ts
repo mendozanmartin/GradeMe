5f69435c5460fb365dc4ca212d95d689444b25de
Martin <mendozanmartin@gmail.com>
2020-02-29 21:45:43 -0500

    fix home page

--- src/app/course-distribution/modal-course-dist/modal-course-dist.component.ts
+++ src/app/course-distribution/modal-course-dist/modal-course-dist.component.ts
@@ -1,21 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalDialogParams } from 'nativescript-angular/common';
+import { Component, OnInit } from "@angular/core";
+import { ModalDialogParams } from "nativescript-angular/common";
 
 @Component({
-  selector: 'ns-modal-course-dist',
-  templateUrl: './modal-course-dist.component.html',
-  styleUrls: ['./modal-course-dist.component.css']
+    selector: "ns-modal-course-dist",
+    templateUrl: "./modal-course-dist.component.html",
+    styleUrls: ["./modal-course-dist.component.css"]
 })
 export class ModalCourseDistComponent implements OnInit {
-  public courseName: string;
-  constructor(private params: ModalDialogParams) {
-    this.courseName = this.params.context.courseName
-  }
+    public courseName: string;
+    constructor(private params: ModalDialogParams) {
+        console.log(this.params.context);
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {}
 
-  close() {
-    // this.params.closeCallback(undefined)
-  }
+    close() {
+        // this.params.closeCallback(undefined)
+    }
 }
